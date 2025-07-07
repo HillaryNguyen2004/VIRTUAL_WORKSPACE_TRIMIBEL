8bed9d6b1b86e161d9c18f6db6ce9e8c6291fb50
HillaryNguyen2004 <hillary.le.2004@gmail.com>
2025-07-07 15:36:38 +0700

    Refactor some more code

--- app/Http/Controllers/UserExportController.php
+++ app/Http/Controllers/UserExportController.php
@@ -1,65 +1,23 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-use Illuminate\Support\Facades\Response;
+use App\Services\UserExportService;
 
 class UserExportController extends Controller
 {
-    public function exportExcel(Request $request)
+    protected $exportService;
+
+    public function __construct(UserExportService $exportService)
     {
-        $spreadsheet = new Spreadsheet();
-        $sheet = $spreadsheet->getActiveSheet();
-
-        // Header row
-        $sheet->fromArray([
-            ['ID', 'Name', 'Email', 'Birthday', 'Created At']
-        ]);
-
-        // Apply filters based on request
-        $query = User::query();
-
-        if ($request->filled('search')) {
-            $query->where(function ($q) use ($request) {
-                $q->where('name', 'like', '%' . $request->search . '%')
-                ->orWhere('email', 'like', '%' . $request->search . '%');
-            });
-        }
-
-        if ($request->filled('role')) {
-            $query->whereHas('roles', function ($q) use ($request) {
-                $q->where('name', $request->role);
-            });
-        }
-
-        // Apply sorting
-        if ($request->filled('sort') && in_array($request->sort, ['asc', 'desc'])) {
-            $query->orderBy('name', $request->sort);
-        }
-
-        $users = $query->select('id', 'name', 'email', 'birthday', 'created_at')->get();
-
-        // Fill data
-        $row = 2;
-        foreach ($users as $user) {
-            $sheet->setCellValue('A' . $row, $user->id);
-            $sheet->setCellValue('B' . $row, $user->name);
-            $sheet->setCellValue('C' . $row, $user->email);
-            $sheet->setCellValue('D' . $row, $user->birthday);
-            $sheet->setCellValue('E' . $row, $user->created_at);
-            $row++;
-        }
-
-        // Export file
-        $filename = 'user_list_' . now()->format('Ymd_His') . '.xlsx';
-        $writer = new Xlsx($spreadsheet);
-        $tempFile = tempnam(sys_get_temp_dir(), $filename);
-        $writer->save($tempFile);
-
-        return response()->download($tempFile, $filename)->deleteFileAfterSend(true);
+        $this->exportService = $exportService;
     }
 
+    public function exportExcel(Request $request)
+    {
+        $users = $this->exportService->getFilteredUsers($request);
+        $export = $this->exportService->generateExcel($users);
+
+        return response()->download($export['file'], $export['name'])->deleteFileAfterSend(true);
+    }
 }
