20bcd17de13acbee502ce7c30831ca396e24a6ce
minhkhoa6868 <trantuanminhkhoa2004@gmail.com>
2026-05-19 13:10:56 +0700

    fix: strip ACL from S3 uploads for bucket owner enforced policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,13 @@ use App\Repositories\UserRepository;
 use Illuminate\Pagination\Paginator;
 use App\Repositories\ProjectRepository;
 use App\Repositories\ProjectRepositoryInterface;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
+use League\Flysystem\Filesystem;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Aws\S3\S3Client;
+use Aws\Middleware;
+use Aws\CommandInterface;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -47,5 +54,37 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useTailwind();
+
+        // S3 bucket has ACLs disabled (Bucket owner enforced).
+        // Strip ACL from every upload so PutObject doesn't get rejected.
+        Storage::extend('s3', function ($app, $config) {
+            $client = new S3Client([
+                'version'     => 'latest',
+                'region'      => $config['region'],
+                'credentials' => [
+                    'key'    => $config['key'],
+                    'secret' => $config['secret'],
+                ],
+                'use_path_style_endpoint' => $config['use_path_style_endpoint'] ?? false,
+            ]);
+
+            $client->getHandlerList()->appendInit(
+                Middleware::mapCommand(function (CommandInterface $cmd) {
+                    if (in_array($cmd->getName(), ['PutObject', 'CreateMultipartUpload'])) {
+                        $cmd->offsetUnset('ACL');
+                    }
+                    return $cmd;
+                }),
+                'remove-acl'
+            );
+
+            $adapter = new AwsS3V3Adapter($client, $config['bucket'], $config['prefix'] ?? '');
+
+            return new FilesystemAdapter(
+                new Filesystem($adapter),
+                $adapter,
+                $config
+            );
+        });
     }
 }
